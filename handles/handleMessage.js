ea3fc1f72dbedb0f455d02b7c33edf5cd31d9a46
ikhgs <mathematiques45@gmail.com>
2024-09-22 20:59:11 +0300

    Update handleMessage.js

--- handles/handleMessage.js
+++ handles/handleMessage.js
@@ -1,11 +1,18 @@
-const commands = require("../commands");
+const commands = require('../commands');
 
-module.exports.handleMessage = async (event, sendMessage) => {
-  const message = event.message.text;
-  const senderID = event.sender.id;
-
-  // Appel à la commande `principe`
-  if (commands.principe) {
-    await commands.principe.onChat({ event, api: { sendMessage } });
-  }
+module.exports = {
+    // Votre logique pour gérer les messages ici
+    onChat: async function({ event, api }) {
+        // Implémentation de la logique de chat ici
+        const command = event.body.toLowerCase();
+        
+        if (command.startsWith('!')) { // Exemple de commande
+            const commandName = command.substring(1);
+            if (commands[commandName]) {
+                await commands[commandName].onChat({ event, api });
+            } else {
+                api.sendMessage("Commande inconnue.", event.threadID);
+            }
+        }
+    }
 };
