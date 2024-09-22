7ce5a5ff1f2dbf7a41117c2b153872f0addf3b19
ikhgs <mathematiques45@gmail.com>
2024-09-22 21:41:23 +0300

    Update sendMessage.js

--- handles/sendMessage.js
+++ handles/sendMessage.js
@@ -1,24 +1,12 @@
-const axios = require("axios");
-
-module.exports.sendMessage = function (recipientId, response) {
-  const request_body = {
-    recipient: {
-      id: recipientId,
-    },
-    message: {
-      text: response,
-    },
-  };
-
-  axios
-    .post(
-      `https://graph.facebook.com/v17.0/me/messages?access_token=${process.env.PAGE_ACCESS_TOKEN}`,
-      request_body
-    )
-    .then(() => {
-      console.log("Message envoyé avec succès !");
-    })
-    .catch((err) => {
-      console.error("Erreur lors de l'envoi du message : " + err);
-    });
+const sendMessage = async (api, message, threadID) => {
+    try {
+        if (!message || !threadID) {
+            throw new Error("Message ou threadID manquant.");
+        }
+        await api.sendMessage(message, threadID);
+    } catch (error) {
+        console.error(`Erreur lors de l'envoi du message: ${error.message}`);
+    }
 };
+
+module.exports = { sendMessage };
